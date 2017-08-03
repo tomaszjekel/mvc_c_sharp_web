5abd9ad34f6d25d4c29e9e586fc7b58b1ac2c83c
Tomasz Jekel <tomaszjekel@gmail.com>
2017-08-03 21:06:17 +0200

    test controller

--- mvc/mvc/App_Start/BundleConfig.cs
+++ mvc/mvc/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace mvc
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/user").Include(
+                      "~/Scripts/homeJs/user.js"));
         }
     }
 }
--- mvc/mvc/Controllers/HomeController.cs
+++ mvc/mvc/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mvc.Models;
+
 
 namespace mvc.Controllers
 {
@@ -25,6 +27,15 @@ namespace mvc.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser (string name)
+        {
+            User user = new User();
+            user.name = name;
+
+            return Json(user, JsonRequestBehavior.AllowGet);
         }
     }
 }
